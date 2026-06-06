df2c48e638b10104dc6468a25d2e5461ebcb8f0b
HafizRP23 <hafiz.tradersfamily@gmail.com>
2026-06-06 22:05:20 +0700

    chore: update application display name and enable cleartext traffic for Android

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Honda Wings',
+      title: 'Aplikasi Kepuasan Pelanggan',
       theme: ThemeData(
         useMaterial3: true,
       ),
